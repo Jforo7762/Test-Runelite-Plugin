7f2ec6b8731dabc7d2d217300fa369b3b980831b
Joe Foroughi <Arman-F@outlook.com>
2024-11-14 02:33:11 -0800

    test rename

--- src/main/java/com/Test/ExamplePlugin.java
+++ src/main/java/com/Test/ExamplePlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.Test;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
--- src/test/java/com/Test/PluginLauncher.java
+++ src/test/java/com/Test/PluginLauncher.java
@@ -1,9 +1,9 @@
-package com.example;
+package com.Test;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class PluginLauncher
 {
 	public static void main(String[] args) throws Exception
 	{
